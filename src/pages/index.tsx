e3da4ac684273deec8f22143fb8076ebd5396d48
Gawacho <22w2021h@shinshu-u.ac.jp>
2022-12-21 16:35:56 +0900

    課題６　ボタンを押すと数字が増えていく画面を作成．

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,29 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import React, { useState } from "react";
 
-const Home: NextPage = () => {
+function App() {
   return (
-    <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-      <h1 className="text-6xl font-bold">ToDolist</h1>
-    </main>
+    <div className="App">
+      <header className="App-header flex w-full flex-1 flex-col items-center justify-center   px-20 text-center">
+        <h1 className="text-6xl font-bold">ToDolist</h1>
+        <LikeButton />
+      </header>
+    </div>
   );
-};
+}
 
-export default Home;
+function LikeButton() {
+  const [count, setCount] = useState(0);
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+  return (
+    <span
+      className="likeButton   bg-red-200 text-white px-4 rounded-0.1xl cursor-pointer justify-center "
+      onClick={handleClick}
+    >
+      {count}
+    </span>
+  );
+}
+
+export default App;
